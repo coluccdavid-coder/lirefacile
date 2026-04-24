3f89fb671f623f83b7d454985c888783a8dbaa09
coluccdavid-coder <colucc.david@gmail.com>
2026-04-24 10:42:21 +0200

    Update metadata description and simplify body tag

--- app/layout.js
+++ app/layout.js
@@ -2,21 +2,13 @@ import "./globals.css";
 
 export const metadata = {
   title: "LireFacile",
-  description: "Plateforme AVC & Dyslexie",
+  description: "Plateforme AVC, Dyslexie et Autisme",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body
-        style={{
-          margin: 0,
-          padding: 0,
-          backgroundColor: "#e5e5e5",
-        }}
-      >
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
