904e0e4243fe45ea42928e9d5714657f3875882f
coluccdavid-coder <colucc.david@gmail.com>
2026-04-24 13:55:36 +0200

    Fix image property assignment in page.js

--- app/exercices/page.js
+++ app/exercices/page.js
@@ -164,7 +164,7 @@ function ExercisesContent() {
         instruction: "Trouve le nombre suivant :",
         question: "2, 4, 6, 8, ___",
         answer: "10",
-        image, null,
+        image: null,
       };
     }
 
